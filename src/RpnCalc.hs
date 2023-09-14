0952cd0915acaab0ec63bc30a5be4aa6202fac43
Günter Hummel <guenter.hummel@mac.com>
2023-09-15 00:42:06 +0200

    Weitere Versuche

--- src/RpnCalc.hs
+++ src/RpnCalc.hs
@@ -3,6 +3,21 @@ module RpnCalc where
 -- solveRPN:: String -> Double
 -- solveRPNinput = ["10", "4", "3", "+", "2", "*","-"]
 
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+-- >>> factorial 20
+-- 2432902008176640000
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+-- >>> circumference 4.0
+-- 25.132742
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+-- >>> circumference' 4.0
+-- 25.132741228718345
+
 doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
@@ -10,7 +25,7 @@ doubleUs :: Num a => a -> a -> a
 doubleUs x y = doubleMe x + doubleMe y
 
 tripleSmallNumber x = if x > 50
-    then x 
+    then x
     else x*3
 
 tripleSmallNumber' x = ( if x > 50  then x else x*3) + 1
@@ -30,7 +45,7 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 -- ["BOOM!","BANG!","BANG!"]
 
 boomBangs' :: Integral a => [a] -> [String]
-boomBangs' xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, even x] 
+boomBangs' xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, even x]
 
 -- >>> [ x | x <- [10..20], x /= 13, x /= 15, x /= 19]
 -- [10,11,12,14,16,17,18,20]
@@ -53,3 +68,77 @@ length' xs = sum [1 | _ <- xs ]
 
 -- >>> length' nouns
 
+multThree :: Int -> (Int -> (Int -> Int))
+multThree x y z = x * y * z
+
+-- >>> multThree 3 5 9     
+-- 135
+
+multTwoWithNine :: Int -> Int -> Int
+multTwoWithNine = multThree 9
+
+-- >>> multTwoWithNine 2 3
+-- 54
+
+
+multOneWithSevenNine :: Int -> Int 
+multOneWithSevenNine = multThree 7 9
+
+-- >>> multOneWithSevenNine 2 
+-- 126
+
+compareWithHundred :: (Ord a, Num a) => a -> Ordering
+compareWithHundred = compare 100
+
+
+compareWithHundred' :: (Ord a, Num a) => a -> Ordering
+compareWithHundred' x = compare x 100
+
+
+isUpperAlphanum  =  (`elem` (['A'..'Z'] ++ ['0'..'9']))
+
+-- >>> isUpperAlphanum 'A'
+-- >>> isUpperAlphanum '1'
+-- >>> isUpperAlphanum 'a'
+-- True
+-- True
+-- False
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+-- >>> applyTwice (+3) 10
+-- 16
+
+-- "Ho Ho Ho"
+-- >>> applyTwice (++ " Ho") "Ho" 
+
+-- >>> applyTwice ("HAHA " ++) "HEY" 
+-- "HAHA HAHA HEY"
+
+
+-- >>> zipWith' (+) [4,2,5,6] [2,6,2,3]
+-- [6,8,7,9]
+
+-- >>> zipWith' (max) [6,3,2,1] [7,3,1,5]
+-- [7,3,2,5]
+
+-- >>> zipWith' max [8,3] [7,5,1]
+-- [8,5]
+
+-- >>> zipWith' (++) ["foo ", "bar ", "baz "] ["fighters", "hoppers", "aldrin"]
+-- ["foo fighters","bar hoppers","baz aldrin"]
+
+-- >>> zipWith' (*) [1,2,3] [3,2,2]
+-- [3,4,6]
+
+-- >>> zipWith' (zipWith' (*)) [[1,2,3],[3,5,6],[2,3,4]] [[3,2,2],[3,4,5],[5,4,3]]
+-- [[3,4,6],[9,20,30],[10,12,12]]
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g where g x y = f y x
