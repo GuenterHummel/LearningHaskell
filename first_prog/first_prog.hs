95991cbad93b1306b7872d5af14bca240d1e492f
Guenter Hummel <guenter.hummel@sbs.co.at>
2023-10-02 11:23:58 +0200

    Update

--- first_prog/first_prog.hs
+++ first_prog/first_prog.hs
@@ -9,10 +9,13 @@ main = do
   author <- getLine
   print (createEmail recipient title author)
 
+toPart :: [Char] -> [Char]
 toPart recipient = "Dear " ++ recipient ++ ",\n"
 bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ "\n"
+fromPart :: [Char] -> [Char]
 fromPart author = "Thanks,\n" ++ author 
 
+createEmail :: [Char] -> [Char] -> [Char] -> [Char]
 createEmail recipient bookTitle author = toPart recipient ++ 
                                          bodyPart bookTitle ++
                                          fromPart author
